4148dac354187fa8aae3354880f8d45c3e8c4322
Trần Gia Huy <trangiahuycit@gmail.com>
2026-08-05 16:32:35 +0700

    feat: implement bcrypt and jwt utility functions for password hashing and token generation

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env.js";
+import { AuthTokenPayload } from "../types/auth.js";
+
+export function generateToken(
+  payload: AuthTokenPayload,
+  expiresIn: string = "1h",
+): string {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn } as jwt.SignOptions);
+}

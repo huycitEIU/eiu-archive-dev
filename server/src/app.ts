2b46f5bb0fa344a65ed51b7689613c4234c11d66
Trần Gia Huy <trangiahuycit@gmail.com>
2026-08-05 12:48:46 +0700

    refactor: clean up code formatting and remove unnecessary dotenv configuration

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,8 @@
 import express from "express";
 import cors from "cors";
 
-// require("dotenv").config();
-import dotenv from "dotenv";
-dotenv.config();
-
 // Import routes
-import healthRoutes from "./routes/healthRoutes.js";    
+import healthRoutes from "./routes/healthRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import documentsRoutes from "./routes/documentsRoutes.js";
 
@@ -20,16 +16,16 @@ app.use(express.json());
 app.use(logRequest);
 
 // Routes test
-app.get('/', (req, res) => {
-    res.json({
-        success: true,
-        message: 'EIU Archive API is running'
-    });
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "EIU Archive API is running",
+  });
 });
 
 // Route Auth API
 app.use(healthRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/documents', documentsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/documents", documentsRoutes);
 
 export default app;
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,10 +1,7 @@
-
 const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined in the environment variables.");
+  throw new Error("JWT_SECRET is not defined in the environment variables.");
 }
 
-export {
-    JWT_SECRET
-};
+export { JWT_SECRET };

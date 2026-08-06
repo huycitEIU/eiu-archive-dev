a1a7780354828d0f911fe7511bdd561cde7f44e5
Trần Gia Huy <trangiahuycit@gmail.com>
2026-08-06 09:28:46 +0700

    refactor: update column layout for Overview page to improve responsiveness

--- client/src/pages/Dashboard/OverviewPage.jsx
+++ client/src/pages/Dashboard/OverviewPage.jsx
@@ -7,29 +7,33 @@ const { Title, Text } = Typography;
 const Overview = () => {
   return (
     <Row gutter={[16, 16]}>
-      <Col xs={24} sm={12} md={8}>
+      <Col xs={24} sm={12} lg={6}>
         <Card>
           <Statistic title="Tổng số tài liệu" value={120} />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8}>
+      <Col xs={24} sm={12} lg={6}>
         <Card>
           <Statistic title="Tổng số danh mục" value={15} />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8}>
+      <Col xs={24} sm={12} lg={6}>
         <Card>
           <Statistic title="Tài liệu mới trong tháng" value={30} />
         </Card>
       </Col>
-
-      <Col xs={24} sm={12} md={8}>
+      <Col xs={24} sm={12} lg={6}>
         <Card>
           <Statistic title="Dung lượng lưu trữ đã sử dụng" value="50 GB" />
         </Card>
       </Col>
-      <Col xs={24} sm={12} md={8}>
-        <Calendar fullscreen={false} />
+
+      <Col xs={24} style={{ marginTop: 8 }}>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Card style={{ width: "100%", maxWidth: 420 }}>
+            <Calendar fullscreen={false} />
+          </Card>
+        </div>
       </Col>
     </Row>
   );

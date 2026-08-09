8537e8aaef4b10cb490c9be8281810ffc1f1adbf
Trần Gia Huy <trangiahuycit@gmail.com>
2026-08-09 20:03:22 +0700

    feat: enhance LoginPage and RegisterPage with responsive design and improved user interface

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -11,6 +11,7 @@ import {
   Space,
   Splitter,
   Layout,
+  Grid,
 } from "antd";
 import type { FormProps } from "antd";
 
@@ -20,6 +21,8 @@ import { Typography } from "antd";
 
 import authService from "../services/authService";
 import { useNavigate } from "react-router-dom";
+
+const { useBreakpoint } = Grid;
 
 type FieldType = {
   username?: string;
@@ -32,10 +35,13 @@ const onFinishFaild: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
 };
 
 const { Text, Title, Paragraph, Link } = Typography;
+
 const LoginPage: React.FC = () => {
   const {
-    token: { colorBgContainer },
+    token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const screens = useBreakpoint();
 
   const navigate = useNavigate();
 
@@ -44,6 +50,106 @@ const LoginPage: React.FC = () => {
     await authService.login(values.username!, values.password!);
     navigate("/dashboard");
   };
+
+  if (!screens.md) {
+    return (
+      <Card
+        style={{
+          margin: "auto",
+          border: 0,
+        }}
+      >
+        <div
+          style={{
+            background: "#144069",
+            textAlign: "center",
+            padding: "16px 24px",
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Title
+            style={{
+              fontSize: "48px",
+              color: "#ffffff",
+            }}
+          >
+            EIU Archive
+          </Title>
+          <Title
+            style={{
+              fontSize: "24px",
+              color: "#efb31d",
+            }}
+          >
+            Learn. Share. Inherit.
+          </Title>
+        </div>
+        <Title level={4}>Welcome Back!</Title>
+        <Paragraph>
+          Sign in to access to dasdboard and finding resources.
+        </Paragraph>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFaild}
+          autoComplete="on"
+          layout="vertical"
+        >
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password size="large" />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            name="remember"
+            valuePropName="checked"
+            label={null}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button block type="primary" htmlType="submit" size="large">
+              Sign In
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <Divider plain>Or</Divider>
+
+        <Space
+          orientation="vertical"
+          size={"medium"}
+          style={{ display: "flex", alignItems: "center", width: "100%" }}
+        >
+          <Button block size="large">
+            <GoogleOutlined /> Countinue with Google
+          </Button>
+          <Text>
+            Don't have an Account?{" "}
+            <Link
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Sign up
+            </Link>{" "}
+          </Text>
+        </Space>
+      </Card>
+    );
+  }
 
   return (
     <Layout
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -10,6 +10,7 @@ import {
   Space,
   Splitter,
   Layout,
+  Grid,
 } from "antd";
 import type { FormProps } from "antd";
 
@@ -20,6 +21,8 @@ import { Typography } from "antd";
 import authService from "../services/authService";
 
 import { useNavigate } from "react-router-dom";
+
+const { useBreakpoint } = Grid;
 
 type FieldType = {
   username?: string;
@@ -35,10 +38,11 @@ const { Text, Title, Paragraph, Link } = Typography;
 
 const RegisterPage: React.FC = () => {
   const {
-    token: { colorBgContainer },
+    token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
   const navigate = useNavigate();
+  const screens = useBreakpoint();
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
     console.log("Success: ", values);
@@ -49,6 +53,105 @@ const RegisterPage: React.FC = () => {
     );
     navigate("/login");
   };
+
+  if (!screens.md) {
+    return (
+      <Card
+        style={{
+          margin: "auto",
+          border: 0,
+        }}
+      >
+        <div
+          style={{
+            background: "#144069",
+            textAlign: "center",
+            padding: "16px 24px",
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Title
+            style={{
+              fontSize: "48px",
+              color: "#ffffff",
+            }}
+          >
+            EIU Archive
+          </Title>
+          <Title
+            style={{
+              fontSize: "24px",
+              color: "#efb31d",
+            }}
+          >
+            Learn. Share. Inherit.
+          </Title>
+        </div>
+        <Title level={4}>Welcome!</Title>
+        <Paragraph>
+          Sign up to access to dasdboard and finding resources.
+        </Paragraph>
+        <Form
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFaild}
+          autoComplete="on"
+          layout="vertical"
+        >
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input size="large" />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password size="large" />
+          </Form.Item>
+
+          <Form.Item label={null}>
+            <Button block type="primary" htmlType="submit" size="large">
+              Sign Up
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <Divider plain>Or</Divider>
+
+        <Space
+          orientation="vertical"
+          size={"medium"}
+          style={{ display: "flex", alignItems: "center", width: "100%" }}
+        >
+          <Button block size="large">
+            <GoogleOutlined /> Countinue with Google
+          </Button>
+          <Text>
+            Already have an Account?{" "}
+            <Link
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Sign in
+            </Link>
+          </Text>
+        </Space>
+      </Card>
+    );
+  }
 
   return (
     <Layout

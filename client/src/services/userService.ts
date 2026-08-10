dd210f28af97943cc26fd6e6f594a3f3a49b8310
Trần Gia Huy <trangiahuycit@gmail.com>
2026-08-10 20:40:06 +0700

    fix: refactor headers assignment in getAllUsers function for clarity

--- client/src/services/userService.ts
+++ client/src/services/userService.ts
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
-const headers = {
-  Authorization: `Bearer ${localStorage.getItem("token")}`,
-};
 
 export const userService = {
   getAllUsers: async (): Promise<{ id: string; username: string }[]> => {
     try {
       const res = await axios.get(`${API_URL}/api/user/all`, {
-        headers: headers,
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       });
       return res.data.users;
     } catch (error) {
